c79b8953b4b84a09563209401b9b2089109bf3e4
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-05-22 15:24:47 +0200

    groundwork for header file and makefile

---
+++ libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 15:07:14 by yridgway          #+#    #+#             */
+/*   Updated: 2022/05/22 15:24:17 by yridgway         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdio.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
